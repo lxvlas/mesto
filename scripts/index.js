aab819f9b5163f865007c7cf3ca9c4ae0db44f38
Aleksey Vlasov <lxvl@yandex.ru>
2022-04-07 23:21:22 +0300

    v.1.2

--- scripts/index.js
+++ scripts/index.js
@@ -3,11 +3,11 @@ const editWindow = document.querySelector('.popup');
 const editCloseBtn = document.querySelector('.popup__close');
 
 function toggleEditWindow() {
-  editWindow.classList.toggle('popup_is-active');
+  editWindow.classList.toggle('popup_opened');
 }
 
 function closeEditWindow() {
-  editWindow.classList.remove('popup_is-active');
+  editWindow.classList.remove('popup_opened');
 }
 
 function onOverLayClick(event) {
@@ -19,3 +19,18 @@ function onOverLayClick(event) {
 editProfile.addEventListener('click', toggleEditWindow);
 editCloseBtn.addEventListener('click', toggleEditWindow);
 editWindow.addEventListener('click', onOverLayClick);
+
+let formElement = document.querySelector('.popup__container');
+let nameInput = formElement.querySelector('.popup__text_name'); 
+let jobInput = formElement.querySelector('.popup__text_job');
+
+function formSubmitHandler (evt) {
+  evt.preventDefault(); 
+  let name = document.querySelector('.profile__title')
+  let job = document.querySelector('.profile__subtitle')
+  name.textContent = nameInput.value;
+  job.textContent = jobInput.value;
+  toggleEditWindow()
+ }
+
+ formElement.addEventListener('submit', formSubmitHandler);
